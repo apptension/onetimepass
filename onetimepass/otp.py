5780f68e7be6c6cf2e9b78ae7511fe70256309ea
Daniel Staśczak <dstasczak@apptension.com>
2021-12-05 11:19:25 +0100

    Use `dict.pop`

--- onetimepass/otp.py
+++ onetimepass/otp.py
@@ -278,8 +278,7 @@ def rename(ctx: click.Context, old_alias: str, new_alias: str):
     db = JSONEncryptedDB(path=settings.DB_PATH, key=keyring_get().encode())
     data = db.read()
     try:
-        data.otp[new_alias] = data.otp[old_alias]
-        del data.otp[old_alias]
+        data.otp[new_alias] = data.otp.pop(old_alias)
         db.write(data)
     except KeyError:
         raise click.UsageError(f"Alias: {old_alias} does not exist")
